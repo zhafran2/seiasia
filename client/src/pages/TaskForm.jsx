447640f72718a4902034c252e0528a633475e1a5
zhafran2 <zhafranm.irsyad@gmail.com>
2025-08-28 10:26:47 +0700

    applied react router

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router';
 import { tasksAPI } from '../services/api';
 import './TaskForm.css';
 
